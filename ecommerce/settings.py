284c311782fcabc0471e946d32dd9eaa0fc3bafb
DeinerSierra <deiner.sierra@gmail.com>
2022-10-02 12:34:54 -0500

    deploy app tienda online

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,7 +155,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (  
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
